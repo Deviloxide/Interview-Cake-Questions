add9458db603fd6e3e074a74502fada90296b196
Deviloxide <55717532+Deviloxide@users.noreply.github.com>
2022-02-17 10:27:41 -0600

    Add files via upload

--- 12. Combinatorics, probability, and other math/Find in Ordered Set.py
+++ 12. Combinatorics, probability, and other math/Find in Ordered Set.py
@@ -1,15 +1,22 @@
 import random
 
+# Code used to test function starts
+# Creates a random integer in the interval [10, 9999] to serves as the length of the list
 limit = random.randint(10, 9999)
 print("This is the generated length of the list:", limit)
 
+# Creates a random insert value in interval [10, limit - 1] to avoid the edge case of having the integer attempt to be
+# inserted out of range
 insert = random.randint(1, limit - 1)
 print("This is the generated value inserted into the list:", insert)
 
+# Uses the random limit integer to create a list of size len(rand_list) == limit - 1
 rand_list = list(range(1, limit))
 
-rand_list.insert(0, insert)
+# Inserts the random limit integer a the end of the list, then shuffles the list
+rand_list.append(insert)
 random.shuffle(rand_list)
+# Code used to test function ends
 
 
 def find_duplicate(num_list):
@@ -19,8 +26,13 @@ def find_duplicate(num_list):
     for num in num_list:
         total += num
 
-    correct_total = (len(num_list) ** 2 + len(num_list)) / 2
+    # Uses the triangle number formula to get the result of summing all integers in over the interval
+    # [1, length] without the need to actually add each individual value
+    correct_total = (length ** 2 + length) / 2
 
+    # Finding the difference between the correct_total and total only results in the distance between the added value
+    # and the length of the list, so finding the difference between length and the original difference gives what must be
+    # the added value
     difference = int(length - (correct_total - total))
 
     return difference
